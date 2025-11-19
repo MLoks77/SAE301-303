c1fea279e0490ba4ff4c7f0c25e9574b18a61a3e
Joachim <joachim.tocqueville@gmail.com>
2025-11-19 12:48:51 +0100

    Premier commit page inscription

--- frontend/sushngo/src/app/app.ts
+++ frontend/sushngo/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from './components/footer/footer';
+import { Inscconnex } from './components/inscconnex/inscconnex';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer],
+  imports: [RouterOutlet, Footer, Inscconnex],
   templateUrl: './app.html',
   styleUrl: 'styles/css/styles.css'
 })
--- frontend/sushngo/src/app/components/inscconnex/inscconnex.ts
+++ frontend/sushngo/src/app/components/inscconnex/inscconnex.ts
@@ -1,11 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-inscconnex',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './inscconnex.html',
   styleUrl: './inscconnex.css',
 })
 export class Inscconnex {
-
 }
