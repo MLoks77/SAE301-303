730fecf87dbb54176d3e1bd181fbc9cb179ed745
Alexouuuu <alexchisiu@gmail.com>
2025-11-24 21:38:41 +0100

    Ajout des chemins angular dans app.routes.ts pour les redirections des pages

--- frontend/sushngo/src/app/app.routes.ts
+++ frontend/sushngo/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { Index } from './components/index/index';
+import { Utilisationcookie } from './components/utilisationcookie/utilisationcookie';
+import { Compte } from './components/compte/compte';
+import { Inscconnex } from './components/inscconnex/inscconnex';
+import { Mdpoublie } from './components/mdpoublie/mdpoublie';
+import { Mentionslegales } from './components/mentionslegales/mentionslegales';
+import { Pagemenu } from './components/menus/pagemenu';
+import { Panier } from './components/panier/panier';
+import { Plansite } from './components/plansite/plansite';
+import { Pointsretraits } from './components/pointsretraits/pointsretraits';
+import { Stats } from './components/stats/stats';
+import { Ugc } from './components/ugc/ugc';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Index },
+  { path: 'utilisation-cookie', component: Utilisationcookie },
+  { path: 'compte', component: Compte },
+  { path: 'inscription-connexion', component: Inscconnex },
+  { path: 'mot-de-passe-oublie', component: Mdpoublie },
+  { path: 'mentions-legales', component: Mentionslegales },
+  { path: 'menu', component: Pagemenu },
+  { path: 'panier', component: Panier },
+  { path: 'plan-site', component: Plansite },
+  { path: 'points-retraits', component: Pointsretraits },
+  { path: 'stats', component: Stats },
+  { path: 'ugc', component: Ugc },
+  { path: '**', redirectTo: 'inscription' } // Route par défaut pour les chemins non trouvés
+];
--- frontend/sushngo/src/app/components/cookiecard/cookiecard.ts
+++ frontend/sushngo/src/app/components/cookiecard/cookiecard.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cookiecard',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './cookiecard.html',
   styleUrl: './cookiecard.css',
 })
 export class Cookiecard {
   accepterCookies() {
     console.log('Les cookies ont été acceptés. Miam !');
+    
   }
   refuserCookies() {
     console.log('Les cookies ont été refusés. Oh non !');
