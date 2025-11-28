34d7a2420b8f0b4601d132d88cef1df2e87ae7df
Maxime <maximederenes@gmail.com>
2025-11-28 14:29:01 +0100

    debug

--- frontend/sushngo/src/app/app.ts
+++ frontend/sushngo/src/app/app.ts
@@ -1,14 +1,7 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet} from '@angular/router';
 import { Footer } from './components/footer/footer';
 import { Navbar } from './components/navbar/navbar';
-<<<<<<< HEAD
-import { Inscconnex } from './components/inscconnex/inscconnex';
-import { Cookiecard } from './components/cookiecard/cookiecard';
-import { Plansite } from './components/plansite/plansite';
-import { Contact } from './components/contact/contact';
-=======
->>>>>>> 507a8e4e731c5d1ee60d08846ecb9ac8ef60710b
 
 @Component({
   selector: 'app-root',
