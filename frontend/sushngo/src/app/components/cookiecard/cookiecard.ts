e1eaf4a67ffec3cda4bde6b809306a86a87be435
Alexouuuu <alexchisiu@gmail.com>
2025-12-05 11:59:52 +0100

    Ajout fonctionnalités cookiecard

--- frontend/sushngo/src/app/components/cookiecard/cookiecard.ts
+++ frontend/sushngo/src/app/components/cookiecard/cookiecard.ts
@@ -1,18 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cookiecard',
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './cookiecard.html',
   styleUrl: './cookiecard.css',
 })
 export class Cookiecard {
+  public message: string = ""; //aucun message affiché car "" par défaut
+
+  public isClosed: boolean = false; //variable pour savoir si cookiecard est fermée (ici non)
+
   accepterCookies() {
-    console.log('Les cookies ont été acceptés. Miam !');
-    
+    this.message = "Les cookies ont été acceptés. Miam !";
+
+    setTimeout(() => { //setTimeout = chronomètre et 2000 = 2000ms (2secondes)
+      this.isClosed = true; //isClosed = true -> Cookiecard disparait
+    }, 2000);
   }
+
   refuserCookies() {
-    console.log('Les cookies ont été refusés. Oh non !');
+    this.message = "Les cookies ont été refusés. Oh non !";
+
+    setTimeout(() => {
+      this.isClosed = true;
+    }, 2000);
   }
 }
