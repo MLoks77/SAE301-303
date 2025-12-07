6c50403e80732745b305ab48cb488d1fbeb8afad
Maxime <maximederenes@gmail.com>
2025-12-07 21:22:44 +0100

    fix routerlink stats

--- frontend/sushngo/src/app/components/stats/stats.ts
+++ frontend/sushngo/src/app/components/stats/stats.ts
@@ -4,7 +4,7 @@ import { Navbar } from '../navbar/navbar';
 import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-stats',
-  imports: [Navbar, Footer],
+  imports: [Navbar, Footer, RouterLink],
   templateUrl: './stats.html',
   styleUrl: './stats.css',
 })
