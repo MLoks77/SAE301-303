44cfacc0f31b80336fd595050bdac47d49be1b6c
Maxime <maximederenes@gmail.com>
2025-12-08 10:29:32 +0100

    fin page accueil

--- frontend/sushngo/src/app/components/accueil/accueil.ts
+++ frontend/sushngo/src/app/components/accueil/accueil.ts
@@ -11,97 +11,102 @@ import { CommonModule } from '@angular/common';
   templateUrl: './accueil.html',
   styleUrl: './accueil.css',
 })
+
 export class Accueil {
 
-  dishes = [
+  plats = [
     {
       id: 1,
       name: 'Toro Sashimi',
-      description: 'Premium fatty tuna belly, delicately sliced.',
+      description: 'Thon premium, finement tranché.',
       price: '€24',
       image:
-        'https://images.unsplash.com/photo-1534482421-64566f976cfa?q=80&w=1000&auto=format&fit=crop',
+        '/images/assets/sushi1.webp',
+      link: '/menus', // mettre un id
     },
     {
       id: 2,
       name: 'Uni Nigiri',
-      description: 'Fresh sea urchin from Hokkaido, crisp nori.',
+      description: 'Oursin frais d’Hokkaido, croustillant et savoureux.',
       price: '€18',
       image:
-        'https://images.unsplash.com/photo-1617196034438-61e8c128373e?q=80&w=1000&auto=format&fit=crop',
+        '/images/assets/sushi2.webp',
+      link: '/menus', // mettre un id
     },
     {
       id: 3,
       name: 'Wagyu Roll',
-      description: 'Seared A5 Wagyu beef, truffle oil, asparagus.',
+      description: 'Bœuf Wagyu poêlé, huile de truffe, asperges.',
       price: '€32',
       image:
-        'https://images.unsplash.com/photo-1553621042-f6e147245754?q=80&w=1000&auto=format&fit=crop',
+        '/images/assets/sushi3.webp',
+      link: '/menus', // mettre un id
     },
     {
       id: 4,
       name: 'Omakase Set',
-      description: "Chef's selection of 12 seasonal pieces.",
+      description: "Sélection du chef : 12 pièces de saison.",
       price: '€85',
       image:
-        'https://images.unsplash.com/photo-1579584425555-c3ce17fd4351?q=80&w=1000&auto=format&fit=crop',
+        '/images/assets/sushi4.webp',
+      link: '/menus', // mettre un id
     },
   ];
 
 
-// ngAfterViewInit parce qu'à ce moment le DOM du composant est prêt ( à tout load )
+  // ngAfterViewInit parce qu'à ce moment le DOM du composant est prêt ( à tout load )
 
-ngAfterViewInit() {
-  const slider = document.getElementById('carousel-scroll');
-  if (!slider) return;
+  ngAfterViewInit() {
+    const slider = document.getElementById('carousel-scroll');
+    if (!slider) return;
 
-  let Bas = false;
-  let debutX: number = 0;
-  let scrollLeft: number = 0;
+    let Bas = false;
+    let debutX: number = 0;
+    let scrollLeft: number = 0;
 
-  // Souris
-  slider.addEventListener('mousedown', (e: MouseEvent) => {
-    Bas = true;
-    slider.classList.add('cursor-grabbing');
-    debutX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
-    e.preventDefault();
-  });
+    // Souris
+    slider.addEventListener('mousedown', (e: MouseEvent) => {
+      Bas = true;
+      slider.classList.add('cursor-grabbing');
+      debutX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+      e.preventDefault();
+    });
 
-  slider.addEventListener('mouseleave', () => {
-    Bas = false;
-    slider.classList.remove('cursor-grabbing');
-  });
+    slider.addEventListener('mouseleave', () => {
+      Bas = false;
+      slider.classList.remove('cursor-grabbing');
+    });
 
-  slider.addEventListener('mouseup', () => {
-    Bas = false;
-    slider.classList.remove('cursor-grabbing');
-  });
+    slider.addEventListener('mouseup', () => {
+      Bas = false;
+      slider.classList.remove('cursor-grabbing');
+    });
 
-  slider.addEventListener('mousemove', (e: MouseEvent) => {
-    if (!Bas) return;
-    e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
-    const walk = (x - debutX) * 1;
-    slider.scrollLeft = scrollLeft - walk;
-  });
+    slider.addEventListener('mousemove', (e: MouseEvent) => {
+      if (!Bas) return;
+      e.preventDefault();
+      const x = e.pageX - slider.offsetLeft;
+      const walk = (x - debutX) * 1;
+      slider.scrollLeft = scrollLeft - walk;
+    });
 
-  let startTouchX = 0;
-  let startScrollLeft = 0;
+    let startTouchX = 0;
+    let startScrollLeft = 0;
 
-  slider.addEventListener('touchstart', (e: TouchEvent) => {
-    if (e.touches.length !== 1) return;
-    startTouchX = e.touches[0].pageX;
-    startScrollLeft = slider.scrollLeft;
-  });
+    slider.addEventListener('touchstart', (e: TouchEvent) => {
+      if (e.touches.length !== 1) return;
+      startTouchX = e.touches[0].pageX;
+      startScrollLeft = slider.scrollLeft;
+    });
 
-  slider.addEventListener('touchmove', (e: TouchEvent) => {
-    if (e.touches.length !== 1) return;
-    const moveX = e.touches[0].pageX;
-    const walk = (moveX - startTouchX) * 1;
-    slider.scrollLeft = startScrollLeft - walk;
-  });
-}
+    slider.addEventListener('touchmove', (e: TouchEvent) => {
+      if (e.touches.length !== 1) return;
+      const moveX = e.touches[0].pageX;
+      const walk = (moveX - startTouchX) * 1;
+      slider.scrollLeft = startScrollLeft - walk;
+    });
+  }
 
 }
 
