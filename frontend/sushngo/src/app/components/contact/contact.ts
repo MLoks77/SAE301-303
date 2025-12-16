21a6e19fcf42a00c3d763f1f87bb56196cdda5d0
Maxime <maximederenes@gmail.com>
2025-12-16 10:02:26 +0100

    push panier début

--- frontend/sushngo/src/app/components/contact/contact.ts
+++ frontend/sushngo/src/app/components/contact/contact.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Navbar } from '../navbar/navbar';
-import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-contact',
-  imports: [ RouterLink , Navbar],
+  imports: [RouterLink, Navbar],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
