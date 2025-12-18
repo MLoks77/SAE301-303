8355b3a0b1539e670f66e347c5d4a1689902598a
Maxime <maximederenes@gmail.com>
2025-12-18 10:20:40 +0100

    remplis pages droits

--- frontend/sushngo/src/app/components/mentionslegales/mentionslegales.ts
+++ frontend/sushngo/src/app/components/mentionslegales/mentionslegales.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Footer } from '../footer/footer';
 import { Navbar } from '../navbar/navbar';
 import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-mentionslegales',
-  imports: [ Footer, Navbar],
+  imports: [Footer, Navbar, CommonModule, RouterLink],
   templateUrl: './mentionslegales.html',
   styleUrl: './mentionslegales.css',
 })
 export class Mentionslegales {
 
-  
+
+  currentTab: string = 'mentions';
+
+  showTab(tab: string) {
+    this.currentTab = tab;
+  }
 }
